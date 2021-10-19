e984981d21393650f655dfca3677e204ae1a4ed0
Maxence <m376ax96@gmail.com>
2021-10-19 14:11:30 +0200

    Ajout de la structure de l'application Restaurant

--- src/App.js
+++ src/App.js
@@ -5,19 +5,48 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <div>
+              <img src={logo} className="App-logo" alt="logo" />
+          </div>
+          <div className="main-menu">
+            <ul className="navigation">
+              <li className="puce"><a href="#">Menu1</a></li>
+              <li className="puce"><a href="#">Menu2</a></li>
+              <li className="puce"><a href="#">Menu3</a></li>
+              <li className="puce"><a href="#">Menu4</a></li>
+              <li className="puce"><a href="#">Menu5</a></li>
+              <li className="puce"><a href="#">Menu6</a></li>
+            </ul>
+          </div>
+          <div>
+              <button className="button" type="button">Profil</button>
+          </div>
       </header>
+
+        <div className="homepage">
+            <div className="presentation">
+                <img src="https://st3.depositphotos.com/9880800/18500/i/600/depositphotos_185007706-stock-photo-view-of-stylish-empty-cafe.jpg"></img>
+                <p>Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text</p>
+            </div>
+            <div className="button-home">
+                <button className="button-reserve">Réserver</button>
+                <button className="button-rejoindre">Rejoindre</button>
+            </div>
+            <div className="list-restaurant">
+                <div>
+
+                </div>
+
+                <div>
+
+                </div>
+
+                <div>
+
+                </div>
+
+            </div>
+        </div>
     </div>
   );
 }
